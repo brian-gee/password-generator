3a86bf8a7402c01c4bc7c93ff2ae931a05f81311
Brian Gee <briangeertsma@gmail.com>
2022-06-25 00:24:53 -0400

    added basic prompts to generatePassword function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment code here
-
+function generatePassword () {
+  alert("Hello! Thanks for choosing Brian's Pass V1.0.")
+  let characters = prompt("Which characters would you like your password to include?\n 1 = Letters only (A-Z) \n 2 = Letters and numbers (A-Z+1-0) \n 3 = Letters, numbers, and special characters (A-Z+1-0+@#!$)");
+  let passwordLength = prompt("How many characters would you like? Choose a number between 8 and 128.");
+  return characters;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
