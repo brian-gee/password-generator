7885798286ddb8c59e381948c3cd7afc66e46a4f
Brian Gee <briangeertsma@gmail.com>
2022-06-28 19:52:33 -0400

    defined characters const

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Assignment code here
+const normalCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberCharacters = "012345789";
+const specialCharacters = "!@#$%^&*";
+
 function generatePassword () {
-  alert("Hello! Thanks for choosing Brian's Pass V1.0.")
   let characters = prompt("Which characters would you like your password to include?\n 1 = Letters only (A-Z) \n 2 = Letters and numbers (A-Z+1-0) \n 3 = Letters, numbers, and special characters (A-Z+1-0+@#!$)");
   let passwordLength = prompt("How many characters would you like? Choose a number between 8 and 128.");
-  return characters;
+
+  return getPassword(characters, passwordLength);
 }
 
 // Get references to the #generate element
