8038ffbe05d52161871a5848d0e6f450724c4c2c
Brian Gee <briangeertsma@gmail.com>
2022-06-28 20:45:23 -0400

    added check for character length

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Assignment code here
+// Defining the characters to be used for generating password
 const normalCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numberCharacters = "012345789";
 const specialCharacters = "!@#$%^&*";
 
+// This will be used to prompt the user on how they would like their password 
+// generated and run the getPassword function to return a random password to the users specifications
 function generatePassword () {
   let characters = prompt("Which characters would you like your password to include?\n 1 = Letters only (A-Z) \n 2 = Letters and numbers (A-Z+1-0) \n 3 = Letters, numbers, and special characters (A-Z+1-0+@#!$)");
   let passwordLength = prompt("How many characters would you like? Choose a number between 8 and 128.");
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Please choose a value from 8 - 128.")
+  }
 
   return getPassword(characters, passwordLength);
 }
