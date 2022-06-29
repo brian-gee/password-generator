bdf9e0e9cc53946402f420399fd5bfa5033e6e39
Brian Gee <briangeertsma@gmail.com>
2022-06-29 01:54:37 -0400

    created better prompt system

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 // Defining the characters to be used for generating password
-const normalCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
+const capitalCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numberCharacters = "012345789";
 const specialCharacters = "!@#$%^&*";
 
 // This will be used to prompt the user on how they would like their password 
 // generated and run the getPassword function to return a random password to the users specifications
 function generatePassword () {
-  let characters = prompt("Which characters would you like your password to include?\n 1 = Letters only (A-Z) \n 2 = Letters and numbers (A-Z+1-0) \n 3 = Letters, numbers, and special characters (A-Z+1-0+@#!$)");
-  let passwordLength = prompt("How many characters would you like? Choose a number between 8 and 128.");
-  while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Please choose a value from 8 - 128.")
-  }
+  characters = getPromptBetween(1, 4, "Which characters would you like your password to include?\n 1 = Lowercase letters only (a-z)\n2 = Uppercase and lowercase letters (a-z, A-Z)\n3 = Letters and numbers (a-z, A-Z, 1-0)\n4 = Letters, numbers, and special characters (A-Z+1-0+@#!$)");
+
+  passwordLength = getPromptBetween(8, 128, "How many characters would you like? Choose a number between 8 and 128.");
 
   return getPassword(characters, passwordLength);
 }
 
 function getPassword(type, length) {
-  var characterList = normalCharacters;
+  var characterList = "";
   var result = "";
 
-  if (type == 2) {
-    characterList += numberCharacters;
+  if (type == 1) {
+    characterList += lowerCharacters;
+  } else if (type == 2) {
+    characterList += lowerCharacters + capitalCharacters;
   } else if (type == 3) {
-    characterList += numberCharacters + specialCharacters;
+    characterList += lowerCharacters + capitalCharacters + numberCharacters;
+  } else if (type == 4) {
+    characterList += lowerCharacters + capitalCharacters + numberCharacters + specialCharacters;
   }
+
   while (length > 0) {
     var index = Math.floor(Math.random() * characterList.length);
     result += characterList[index];
     length--;
   }
+
   return result;
+}
+
+function getPromptBetween(min, max, userPropmt){
+  let input = prompt(userPropmt);
+
+  while (input < min || input > max) {
+    alert("Enter a number between" + min + " and " + max);
+    input = prompt(userPropmt);
+  }
+
+  return input;
 }
 
 // Get references to the #generate element
@@ -41,7 +57,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
